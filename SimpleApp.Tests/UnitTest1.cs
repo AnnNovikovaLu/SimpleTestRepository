70eea2f276ec427da75eb4a9a0dcd6d6f27e1c6c
Новикова Аня <novikova_ann.9@mail.ru>
2025-04-16 11:15:01 +0300

    feat:all project is done

---
+++ SimpleApp.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+namespace SimpleApp.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Add_ReturnsCorrectSum()
+        {
+            var calc = new Calculator();
+            var result = calc.Add(2, 3);
+            Assert.Equal(5, result);
+        }
+    }
+}
---
+++ SimpleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace SimpleApp
+{
+    public class Calculator
+    {
+        public int Add(int a, int b) => a + b;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calc = new Calculator();
+            Console.WriteLine($"3 + 5 = {calc.Add(3, 5)}");
+        }
+    }
+}
